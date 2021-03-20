23eb0c3a149a1ddbb4f66d947eade269d2ac4607
Dayana Mick <dayanamick@gmail.com>
2021-03-20 10:58:07 +0100

    begin project

---
+++ src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,21 @@
+<template>
+  <li>
+    <div>
+      <header>
+        <h3>{{title}}</h3>
+        <button>Delete</button>
+      </header>
+    </div>
+    <p>{{description}}</p>
+    <nav>
+      <a :href="link">View Resource</a> 
+      <!-- QUESTION: why does the link have to be bound with : , different from the other props?  -->
+    </nav>
+  </li>
+</template>
+
+<script>
+export default {
+  props:['title', 'description', 'link']
+}
+</script>
